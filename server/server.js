b9e3cb73592ba03bf5fe36457aa3c84c5d8648ee
wawerusam <waweruzamuel@gmail.com>
2026-09-09 14:14:14 +0300

    removed unwanted comments in the server file...

--- server/server.js
+++ server/server.js
@@ -32,28 +32,3 @@ app.listen(PORT, () => {
 //     if (error) console.error(`Error connecting to database: ${error.message}`);
 //   }
 // })();
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = process.env.DB_CONNECTION
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri);
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     const mongoConnection = await client.connect();
-//     // Send a ping to confirm a successful connection
-//     if(!mongoConnection) throw new Error(mongoConnection)
-
-//     const database= await client.db("lipa-rent");
-//     if(database)console.log("You successfully connected to MongoDB!");
-
-//   } catch (error) {
-//     if (error) console.error(`Error connecting to database: ${error.message}`);
-// }finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-
-// run().catch(console.dir)
