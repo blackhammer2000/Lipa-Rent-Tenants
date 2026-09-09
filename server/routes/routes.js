a6f2d39c9eddfc5b1ab04761c9cab24ae5059746
wawerusam <waweruzamuel@gmail.com>
2026-09-09 14:08:20 +0300

    adding more files to the structure...

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,3 +1,9 @@
 const router = require("express").Router();
 
+const postRoutes = require("./post");
+const patchRoutes = require("./patch");
+
+router.use(postRoutes);
+router.use(patchRoutes);
+
 module.exports = router;
